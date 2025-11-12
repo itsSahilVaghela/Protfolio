d2f7a0e2561dcac454e15fc8c7c3ee5145acd2ed
itsSahilVaghela <sahilvaghela.if2@gmail.com>
2025-11-12 16:12:09 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
-
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  // 👇 this must match your GitHub repo name
+  base: "/Protfolio/",
   server: {
     host: "::",
-    port: 8080,
+    port: 8088,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
